44d015a004040adadc1cef966fe0597616cac116
James <a0970953399@gmail.com>
2026-03-22 00:26:24 +0800

    Initialize Firebase with configuration
    Set up Firebase configuration and initialization.

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdctp8WjTwgLwR5ZTAobjQ-ApkNxfbRuw",
+  authDomain: "aristo-cpa-work-platform.firebaseapp.com",
+  projectId: "aristo-cpa-work-platform",
+  storageBucket: "aristo-cpa-work-platform.firebasestorage.app",
+  messagingSenderId: "813300838301",
+  appId: "1:813300838301:web:4f74800c4be5f731930974"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
